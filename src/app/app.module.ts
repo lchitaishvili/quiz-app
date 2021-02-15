286a87798dd37d7bac98810562762cfdebf6e1d3
lchitaishvili <lchitaishvili@gmail.com>
2021-02-15 18:55:08 +0400

    feat: add ngxs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedWithAppModule } from './shared-with-app/shared-with-app.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NgxsModule } from '@ngxs/store';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
+
   imports: [
+    NgxsModule.forRoot([], { developmentMode: !environment.production }),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
     SharedWithAppModule,
